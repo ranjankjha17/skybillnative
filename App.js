a4c09869b859eea6f91af77460321c1c7c083bc5
Ranjan Kumar Jha <talk2ranjanktr@gmail.com>
2023-12-16 19:23:41 +0530

    add error boundary

--- App.js
+++ App.js
@@ -4,11 +4,22 @@ import { Dashboard } from './pages/Dashboard';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Login } from './pages/Login';
+import {ErrorBoundary} from 'react-error-boundary'
 
 const Stack = createStackNavigator();
+function ErrorHandler() {
+  return (
+    <View role="alert">
+      <Text>An error occurred:</Text>
+      {/* <Text>{error.message}</Text> */}
+     
+    </View>
+  )
+}
 
 export default function App() {
   return (
+    <ErrorBoundary FallbackComponent={ErrorHandler}>
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
@@ -17,5 +28,7 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
+    </ErrorBoundary>
+
   );
 }
